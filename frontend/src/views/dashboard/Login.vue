2f805a71bbdeb9c2c99449d3a28954e9e542d54f
nabiilahnadaa <111941049+nabiilahnadaa@users.noreply.github.com>
2022-12-06 21:34:29 +0700

    nyoba template sign in

--- frontend/src/views/dashboard/Login.vue
+++ frontend/src/views/dashboard/Login.vue
@@ -1,146 +1,96 @@
 <template>
-  <v-app id="inspire">
-    <v-content>
-      <v-container class="fill-height" fluid>
-        <v-row align="center" justify="center">
-          <v-col cols="12" sm="8" md="8">
-            <v-card class="elevation-12">
-              <v-window v-model="step">
-                <v-window-item :value="1">
-                  <v-row>
-                    <v-col cols="12" md="8">
-                      <v-card-text class="mt-12">
-                        <h1
-                          class="text-center display-2 teal--text text--accent-3"
-                        >Sign in to Diprella</h1>
-                        <div class="text-center mt-4">
-                          <v-btn class="mx-2" fab color="black" outlined>
-                            <v-icon>fab fa-facebook-f</v-icon>
-                          </v-btn>
-
-                          <v-btn class="mx-2" fab color="black" outlined>
-                            <v-icon>fab fa-google-plus-g</v-icon>
-                          </v-btn>
-                          <v-btn class="mx-2" fab color="black" outlined>
-                            <v-icon>fab fa-linkedin-in</v-icon>
-                          </v-btn>
-                        </div>
-                        <h4 class="text-center mt-4">Ensure your email for registration</h4>
-                        <v-form>
-                          <v-text-field
-                            label="Email"
-                            name="Email"
-                            prepend-icon="email"
-                            type="text"
-                            color="teal accent-3"
-                          />
-
-                          <v-text-field
-                            id="password"
-                            label="Password"
-                            name="password"
-                            prepend-icon="lock"
-                            type="password"
-                            color="teal accent-3"
-                          />
-                        </v-form>
-                        <h3 class="text-center mt-4">Forgot your password ?</h3>
-                      </v-card-text>
-                      <div class="text-center mt-3">
-                        <v-btn rounded color="teal accent-3" dark>SIGN IN</v-btn>
-                      </div>
-                    </v-col>
-                    <v-col cols="12" md="4" class="teal accent-3">
-                      <v-card-text class="white--text mt-12">
-                        <h1 class="text-center display-1">Hello, Friend!</h1>
-                        <h5
-                          class="text-center"
-                        >Enter your personal details and start journay with us</h5>
-                      </v-card-text>
-                      <div class="text-center">
-                        <v-btn rounded outlined dark @click="step++">SIGN UP</v-btn>
-                      </div>
-                    </v-col>
-                  </v-row>
-                </v-window-item>
-                <v-window-item :value="2">
-                  <v-row class="fill-height">
-                    <v-col cols="12" md="4" class="teal accent-3">
-                      <v-card-text class="white--text mt-12">
-                        <h1 class="text-center display-1">Welcome Back!</h1>
-                        <h5
-                          class="text-center"
-                        >To Keep connected with us please login with your personnel info</h5>
-                      </v-card-text>
-                      <div class="text-center">
-                        <v-btn rounded outlined dark @click="step--">Sign in</v-btn>
-                      </div>
-                    </v-col>
-
-                    <v-col cols="12" md="8">
-                      <v-card-text class="mt-12">
-                        <h1 class="text-center display-2 teal--text text--accent-3">Create Account</h1>
-                        <div class="text-center mt-4">
-                          <v-btn class="mx-2" fab color="black" outlined>
-                            <v-icon>fab fa-facebook-f</v-icon>
-                          </v-btn>
-
-                          <v-btn class="mx-2" fab color="black" outlined>
-                            <v-icon>fab fa-google-plus-g</v-icon>
-                          </v-btn>
-                          <v-btn class="mx-2" fab color="black" outlined>
-                            <v-icon>fab fa-linkedin-in</v-icon>
-                          </v-btn>
-                        </div>
-                        <h4 class="text-center mt-4">Ensure your email for registration</h4>
-                        <v-form>
-                          <v-text-field
-                            label="Name"
-                            name="Name"
-                            prepend-icon="person"
-                            type="text"
-                            color="teal accent-3"
-                          />
-                          <v-text-field
-                            label="Email"
-                            name="Email"
-                            prepend-icon="email"
-                            type="text"
-                            color="teal accent-3"
-                          />
-
-                          <v-text-field
-                            id="password"
-                            label="Password"
-                            name="password"
-                            prepend-icon="lock"
-                            type="password"
-                            color="teal accent-3"
-                          />
-                        </v-form>
-                      </v-card-text>
-                      <div class="text-center mt-n5">
-                        <v-btn rounded color="teal accent-3" dark>SIGN UP</v-btn>
-                      </div>
-                    </v-col>
-                  </v-row>
-                </v-window-item>
-              </v-window>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-content>
+  <v-app>
+    <div class="backgruond"></div>
+    <v-main class="d-flex justify-center align-center">
+      <v-col cols="10" lg="4" class="mx-auto">
+        <v-card class="pa-4">
+          <div class="text-center">
+            <v-avatar size="100" color="indigo lighten-4">
+              <v-icon size="40" color="indigo">mdi-account</v-icon>
+            </v-avatar>
+            <h2 class="indigo--text">Vue login Page</h2>
+          </div>
+          <v-form @submit.prevent="submitHandler" ref="form">
+            <v-card-text>
+              <v-text-field
+                v-model="email"
+                :rules="emailRules"
+                type="email"
+                label="Email"
+                placeholder="Email"
+                prepend-inner-icon="mdi-account"
+                required
+              />
+              <v-text-field
+                      v-model="password"
+                      :rules="passwordRules"
+                      :type="passwordShow?'text':'password'"
+                      label="Password"
+                      placeholder="Password"
+                      prepend-inner-icon="mdi-key"
+                      :append-icon="passwordShow ? 'mdi-eye':'mdi-eye-off'"
+                      @click:append="passwordShow = !passwordShow"
+                      required
+              />
+              <v-switch label="Remember me" color="indigo"></v-switch>
+            </v-card-text>
+            <v-card-actions class="justify-center">
+              <v-btn :loading="loading" type="submit" color="indigo">
+                <span class="white--text px-8">Login</span>
+              </v-btn>
+            </v-card-actions>
+          </v-form>
+        </v-card>
+      </v-col>
+    </v-main>
+    <v-snackbar top color="green" v-model="snackbar">
+      Login success
+    </v-snackbar>
   </v-app>
 </template>
 
 <script>
+
 export default {
+  name: 'App',
+
+
   data: () => ({
-    step: 1
+    loading:false,
+    snackbar:false,
+    passwordShow:false,
+    email: '',
+    emailRules: [
+      v => !!v || 'E-mail is required',
+      v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+    ],
+    password: '',
+    passwordRules: [
+      v => !!v || 'Password is required',
+      v => (v && v.length >= 6) || 'Password must be 6  characters or more!',
+    ],
   }),
-  props: {
-    source: String
+  methods:{
+    submitHandler(){
+      if (this.$refs.form.validate()){
+          this.loading = true
+        setTimeout(()=> {
+          this.loading = false
+          this.snackbar = true
+        },3000)
+      }
+    }
   }
 };
 </script>
+<style>
+  .backgruond{
+    background-image: url(./assets/Order-Banner.jpg) !important;
+    height: 300px;
+    width: 100%;
+    display: block;
+    position: absolute;
+    top: 0;
+    background-size: cover;
+  }
+</style>
