5921a00f070b9a946c8f39bf49c59bb815d9a094
nabiilahnadaa <111941049+nabiilahnadaa@users.noreply.github.com>
2022-12-06 20:31:57 +0700

    Update Login.vue

--- frontend/src/views/dashboard/Login.vue
+++ frontend/src/views/dashboard/Login.vue
@@ -1,266 +1,146 @@
-<!-- <template>
-    <div id="login">
-      <h1 class="greeting"><span>W</span>elcome</h1>
-      <h2>Vue Bullet Journal</h2>
-      <div class="entry-options">
-        <h3 @click="entryLogin = true">Log In</h3>
-        <p>/</p>
-        <h3 @click="entryLogin = false">Sign Up</h3>
-      </div>
-      <div class="form-container">
-        <div class="action-boxes" :class="{ front: entryLogin }">
-          <div class="header"><h1>Log In</h1></div>
-          <form @submit.prevent="loggingIn">
-            <div>
-              <label for="loginEmail"><h3>Email</h3></label>
-              <input
-                type="email"
-                id="loginEmail"
-                v-model="loginInfo.email"
-                required
-              />
-              <label for="loginPassword"><h3>Password</h3></label>
-              <input
-                :type="showLoginPW ? 'text' : 'password'"
-                id="loginPassword"
-                v-model="loginInfo.password"
-                placeholder="Minimum 5 characters"
-                minLength="5"
-                required
-              />
-              <h6
-                @click="showLoginPW = !showLoginPW"
-                :class="{ active: showLoginPW }"
-              >
-                Show password
-              </h6>
-            </div>
-            <button type="submit">Log In</button>
-          </form>
-        </div>
-        <div class="action-boxes" :class="{ front: !entryLogin }">
-          <div class="header"><h1>Sign Up</h1></div>
-          <form @submit.prevent="signingUp">
-            <div>
-              <label for="signUpEmail"><h3>Email</h3></label>
-              <input
-                type="email"
-                id="signUpEmail"
-                v-model="signUpInfo.email"
-                required
-              />
-              <label for="signUpPassword"><h3>Password</h3></label>
-              <input
-                :type="showSignUpPW ? 'text' : 'password'"
-                id="signUpPassword"
-                v-model="signUpInfo.password"
-                placeholder="Minimum 5 characters"
-                minLength="5"
-                required
-              />
-              <label for="signUpVerify"><h3>Re-enter Password</h3></label>
-              <input
-                :type="showSignUpPW ? 'text' : 'password'"
-                id="signUpVerify"
-                v-model="signUpInfo.verifyPw"
-                required
-              />
-              <h6
-                @click="showSignUpPW = !showSignUpPW"
-                :class="{ active: showSignUpPW }"
-              >
-                Show passwords
-              </h6>
-            </div>
-            <button type="submit">Sign Up</button>
-          </form>
-        </div>
-      </div>
-      <h4 class="message">{{ getErrorMessage }}</h4>
-      <h4 class="message" v-if="!matchingPW">
-        The passwords entered do not match. Please try again.
-      </h4>
-    </div>
-  </template>
-  
-  <script>
-  import { mapGetters, mapActions } from "vuex";
-  
-  export default {
-    name: "Login",
-    computed: {
-      ...mapGetters(["getErrorMessage"]),
-    },
-    data() {
-      return {
-        loginInfo: {
-          email: "",
-          password: "",
-        },
-        showLoginPW: false,
-        signUpInfo: {
-          email: "",
-          password: "",
-          verifyPw: "",
-        },
-        showSignUpPW: false,
-        matchingPW: true,
-        entryLogin: true,
-      };
-    },
-    methods: {
-      ...mapActions(["loginUser", "signUpUser", "changeErrorMessage"]),
-      loggingIn() {
-        this.loginUser({
-          email: this.loginInfo.email,
-          password: this.loginInfo.password,
-        });
-      },
-      signingUp() {
-        if (this.signUpInfo.password === this.signUpInfo.verifyPw) {
-          this.matchingPW = true;
-  
-          this.signUpUser({
-            email: this.signUpInfo.email,
-            password: this.signUpInfo.password,
-          });
-        } else {
-          this.matchingPW = false;
-          this.changeErrorMessage("");
-        }
-      },
-    },
-  };
-  </script>
-  
-  <style scoped>
-  h1 {
-    margin: 0 auto 10rem;
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="8">
+            <v-card class="elevation-12">
+              <v-window v-model="step">
+                <v-window-item :value="1">
+                  <v-row>
+                    <v-col cols="12" md="8">
+                      <v-card-text class="mt-12">
+                        <h1
+                          class="text-center display-2 teal--text text--accent-3"
+                        >Sign in to Diprella</h1>
+                        <div class="text-center mt-4">
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-facebook-f</v-icon>
+                          </v-btn>
+
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-google-plus-g</v-icon>
+                          </v-btn>
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-linkedin-in</v-icon>
+                          </v-btn>
+                        </div>
+                        <h4 class="text-center mt-4">Ensure your email for registration</h4>
+                        <v-form>
+                          <v-text-field
+                            label="Email"
+                            name="Email"
+                            prepend-icon="email"
+                            type="text"
+                            color="teal accent-3"
+                          />
+
+                          <v-text-field
+                            id="password"
+                            label="Password"
+                            name="password"
+                            prepend-icon="lock"
+                            type="password"
+                            color="teal accent-3"
+                          />
+                        </v-form>
+                        <h3 class="text-center mt-4">Forgot your password ?</h3>
+                      </v-card-text>
+                      <div class="text-center mt-3">
+                        <v-btn rounded color="teal accent-3" dark>SIGN IN</v-btn>
+                      </div>
+                    </v-col>
+                    <v-col cols="12" md="4" class="teal accent-3">
+                      <v-card-text class="white--text mt-12">
+                        <h1 class="text-center display-1">Hello, Friend!</h1>
+                        <h5
+                          class="text-center"
+                        >Enter your personal details and start journay with us</h5>
+                      </v-card-text>
+                      <div class="text-center">
+                        <v-btn rounded outlined dark @click="step++">SIGN UP</v-btn>
+                      </div>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+                <v-window-item :value="2">
+                  <v-row class="fill-height">
+                    <v-col cols="12" md="4" class="teal accent-3">
+                      <v-card-text class="white--text mt-12">
+                        <h1 class="text-center display-1">Welcome Back!</h1>
+                        <h5
+                          class="text-center"
+                        >To Keep connected with us please login with your personnel info</h5>
+                      </v-card-text>
+                      <div class="text-center">
+                        <v-btn rounded outlined dark @click="step--">Sign in</v-btn>
+                      </div>
+                    </v-col>
+
+                    <v-col cols="12" md="8">
+                      <v-card-text class="mt-12">
+                        <h1 class="text-center display-2 teal--text text--accent-3">Create Account</h1>
+                        <div class="text-center mt-4">
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-facebook-f</v-icon>
+                          </v-btn>
+
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-google-plus-g</v-icon>
+                          </v-btn>
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-linkedin-in</v-icon>
+                          </v-btn>
+                        </div>
+                        <h4 class="text-center mt-4">Ensure your email for registration</h4>
+                        <v-form>
+                          <v-text-field
+                            label="Name"
+                            name="Name"
+                            prepend-icon="person"
+                            type="text"
+                            color="teal accent-3"
+                          />
+                          <v-text-field
+                            label="Email"
+                            name="Email"
+                            prepend-icon="email"
+                            type="text"
+                            color="teal accent-3"
+                          />
+
+                          <v-text-field
+                            id="password"
+                            label="Password"
+                            name="password"
+                            prepend-icon="lock"
+                            type="password"
+                            color="teal accent-3"
+                          />
+                        </v-form>
+                      </v-card-text>
+                      <div class="text-center mt-n5">
+                        <v-btn rounded color="teal accent-3" dark>SIGN UP</v-btn>
+                      </div>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+              </v-window>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    step: 1
+  }),
+  props: {
+    source: String
   }
-  
-  h2 {
-    text-align: center;
-  }
-  
-  h3 {
-    color: rgb(25, 81, 94);
-    font-weight: 500;
-  }
-  
-  h6 {
-    cursor: pointer;
-    display: inline-block;
-  }
-  
-  h6.active {
-    color: rgba(3, 190, 3);
-  }
-  
-  button {
-    position: absolute;
-    width: 93%;
-    bottom: 0;
-  }
-  
-  form {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    height: 80%;
-  }
-  
-  input {
-    margin: 5rem 0 10rem;
-  }
-  
-  #login {
-    width: 100%;
-    height: 90vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding-top: 5%;
-  }
-  
-  .greeting {
-    font-size: 35rem;
-    color: rgb(25, 81, 94);
-  }
-  
-  .greeting span {
-    font-size: 38rem;
-  }
-  
-  .entry-options {
-    display: none;
-  }
-  
-  .action-boxes {
-    width: 90%;
-    max-width: 400rem;
-    height: 350rem;
-    background: white;
-  }
-  
-  .form-container {
-    position: relative;
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(350rem, 1fr));
-    width: 100%;
-    grid-gap: 20rem;
-    justify-items: center;
-    padding: 5% 0;
-  }
-  
-  .message {
-    width: 80%;
-    text-align: center;
-  }
-  
-  @media only screen and (min-width: 700px) {
-    .form-container {
-      width: 60%;
-      height: 500rem;
-    }
-  }
-  
-  @media only screen and (max-width: 700px), (max-height: 850px) {
-    h3 {
-      font-size: 12rem;
-    }
-  
-    .greeting {
-      font-size: 20rem;
-    }
-  
-    .greeting span {
-      font-size: 30rem;
-    }
-  
-    .entry-options {
-      display: flex;
-      width: 50%;
-      max-width: 300rem;
-      justify-content: space-between;
-      align-items: center;
-      margin: 20rem 0;
-    }
-  
-    .entry-options h3 {
-      cursor: pointer;
-    }
-  
-    .form-container {
-      height: 400rem;
-    }
-  
-    .action-boxes {
-      position: absolute;
-      width: 80%;
-    }
-  
-    .front {
-      z-index: 10;
-    }
-  }
-  </style> -->
+};
+</script>
