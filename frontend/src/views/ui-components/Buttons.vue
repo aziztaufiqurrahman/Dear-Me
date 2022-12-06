18e139485d0b5919fd36bff064b8afa7c35833a1
nabiilahnadaa <111941049+nabiilahnadaa@users.noreply.github.com>
2022-12-06 15:54:22 +0700

    about us

--- frontend/src/views/ui-components/Buttons.vue
+++ frontend/src/views/ui-components/Buttons.vue
@@ -1,51 +1,140 @@
 <template>
-  <v-card
-    :loading="loading"
-    class="mx-auto my-12"
-    max-width="374"
-  >
-    <template slot="progress">
-      <v-progress-linear
-        color="deep-purple"
-        height="10"
-        indeterminate
-      ></v-progress-linear>
-    </template>
+  <v-flex d-flex xs12 sm6 md4>
+ <v-card
+   class="mx-auto my-12"
+   max-width="374"
+ >
+   <template slot="progress">
+     <v-progress-linear
+       color="deep-purple"
+       height="10"
+       indeterminate
+     ></v-progress-linear>
+   </template>
 
-    <v-img
-      height="250"
-      src="https://akademik.polban.ac.id/fotomhsrekap/201524010.jpg"
-    ></v-img>
+   <v-img
+     height="250"
+     src="https://akademik.polban.ac.id/fotomhsrekap/201524010.jpg"
+   ></v-img>
 
-    <v-card-title>
-      <v-row
-        class="justify-center"
-      >Hasanah
-      </v-row>
-    </v-card-title>
+   <v-card-title>
+     <v-row
+       class="justify-center"
+     >Hasanah
+     </v-row>
+   </v-card-title>
 
-    <v-card-text>
-      <v-row
-        class="justify-center"
-      >201524010
-      </v-row>
-    </v-card-text>
-  </v-card>
+   <v-card-text>
+     <v-row
+       class="justify-center"
+     >201524010
+     </v-row>
+   </v-card-text>
+ </v-card>
+</v-flex>
+
+<v-flex d-flex xs12 sm6 md4>
+ <v-card
+   class="mx-auto my-12"
+   max-width="374"
+ >
+   <template slot="progress">
+     <v-progress-linear
+       color="deep-purple"
+       height="10"
+       indeterminate
+     ></v-progress-linear>
+   </template>
+
+   <v-img
+     height="250"
+     src="https://akademik.polban.ac.id/fotomhsrekap/201524010.jpg"
+   ></v-img>
+
+   <v-card-title>
+     <v-row
+       class="justify-center"
+     >Hasanah
+     </v-row>
+   </v-card-title>
+
+   <v-card-text>
+     <v-row
+       class="justify-center"
+     >201524010
+     </v-row>
+   </v-card-text>
+ </v-card>
+</v-flex>
+
+<v-flex d-flex xs12 sm6 md4>
+ <v-card
+   class="mx-auto my-12"
+   max-width="374"
+ >
+   <template slot="progress">
+     <v-progress-linear
+       color="deep-purple"
+       height="10"
+       indeterminate
+     ></v-progress-linear>
+   </template>
+
+   <v-img
+     height="250"
+     src="https://akademik.polban.ac.id/fotomhsrekap/201524010.jpg"
+   ></v-img>
+
+   <v-card-title>
+     <v-row
+       class="justify-center"
+     >Hasanah
+     </v-row>
+   </v-card-title>
+
+   <v-card-text>
+     <v-row
+       class="justify-center"
+     >201524010
+     </v-row>
+   </v-card-text>
+ </v-card>
+</v-flex>
+
+<v-flex d-flex xs12 sm6 md4>
+ <v-card
+   class="mx-auto my-12"
+   max-width="374"
+ >
+   <template slot="progress">
+     <v-progress-linear
+       color="deep-purple"
+       height="10"
+       indeterminate
+     ></v-progress-linear>
+   </template>
+
+   <v-img
+     height="250"
+     src="https://akademik.polban.ac.id/fotomhsrekap/201524010.jpg"
+   ></v-img>
+
+   <v-card-title>
+     <v-row
+       class="justify-center"
+     >Hasanah
+     </v-row>
+   </v-card-title>
+
+   <v-card-text>
+     <v-row
+       class="justify-center"
+     >201524010
+     </v-row>
+   </v-card-text>
+ </v-card>
+</v-flex>
 </template>
 
-<script>
-  export default {
-    data: () => ({
-      loading: false,
-      selection: 1,
-    }),
 
-    methods: {
-      reserve () {
-        this.loading = true
 
-        setTimeout(() => (this.loading = false), 2000)
-      },
-    },
-  }
-</script>
